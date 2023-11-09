d5cfd3dcc36f7de7a04d6e6e747e3bd4bbc78994
tuananhpaddy <tuananhtran.xd@gmail.com>
2023-11-09 11:37:45 +0700

    them file moi

---
+++ sd.js
@@ -0,0 +1,8 @@
+let inputDai;
+let inputRong;
+inputDai = prompt("nhap chieu dai");
+inputRong = prompt("nhap chieu rong");
+let D= parseInt(inputDai);
+let R= parseInt(inputRong);
+let area= D*R;
+document.write("Dien tich: "+ area);
