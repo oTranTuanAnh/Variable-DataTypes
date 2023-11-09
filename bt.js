44a01ffe30ba92c3845fb094ab86bc5f59c84f7c
tuananhpaddy <tuananhtran.xd@gmail.com>
2023-11-09 11:23:30 +0700

    them moi

---
+++ bt.js
@@ -0,0 +1,37 @@
+function nhapTen() {
+    let ten= "abc";
+    ten = prompt("nhap ten vao day")
+    alert(ten);
+
+}
+
+
+
+
+
+
+
+
+
+// let i=10
+// let f=20.5
+// let b=true
+// let s= "Hà Nội"
+// document.write('i= ' +i)
+// document.write('<br>')
+// document.write('f= ' +f)
+// document.write('<br>')
+// document.write('b= ' +b)
+// document.write('<br>')
+// document.write('s= ' +s)
+//
+// let width = 20
+// let height = 10
+// let area = width * height
+// document.write('Area = ' + area)
+
+// let a = prompt("nhập giá trị của a")
+// let b = prompt("nhập giá trị của b")
+// if (Number(a)%Number(b)==0) {
+//     alert("a chia het cho b")
+// } else { alert("a không chia het cho b")}
